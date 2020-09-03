af5499d021e9353d0ea4ccf8716949fa7007a9d2
Pkaile <69093808+Pkaile@users.noreply.github.com>
2020-09-02 18:34:49 -0700

    first commit

---
+++ precache-manifest.aee58bc7ba1c3c35e17a80dc4756fa9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b969fa9882858f0db1272f5c12405128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3cf70b95eb462ef6dae",
+    "url": "/static/css/main.61d12f4a.chunk.css"
+  },
+  {
+    "revision": "c100c758434c23991f29",
+    "url": "/static/js/2.dfb88666.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dfb88666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cf70b95eb462ef6dae",
+    "url": "/static/js/main.d2ec8b40.chunk.js"
+  },
+  {
+    "revision": "cbe25348f6437431d000",
+    "url": "/static/js/runtime-main.94cd35a6.js"
+  }
+]);
